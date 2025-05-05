67c3bad8d3ca8647bbb72a6f743b2319f4f78ab0
bigxuanfeng <1714632509@qq.com>
2025-05-05 16:51:57 +0800

    Add files via upload

---
+++ my_blog/js/scripts.js
@@ -0,0 +1,9 @@
+// 这里可以添加一些简单的JavaScript代码，例如导航菜单的交互效果
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            // 这里可以添加一些点击事件处理逻辑
+        });
+    });
+});
